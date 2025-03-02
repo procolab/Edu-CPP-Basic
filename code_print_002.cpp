bf4daee0c71ad311c474b1fed41e9eba0617a32c
Himanshu <37922176+procolab@users.noreply.github.com>
2025-03-02 18:42:57 +0530

    Create code_print_002.cpp
    Output:
    3
    6
    10
    Hello World!
    I am learning C++
    === Code Execution Successful ===

---
+++ code_print_002.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << 3 << "\n";
+  // Value print with new line.
+  cout << 3 + 3 << endl;
+  // Add operation with new line.
+  cout << 2 * 5 << "\n";
+  // Mul operation with new line.
+  cout << "Hello World!" << endl;
+  // Simple statment with new line.
+  cout << "I am learning C++" << "\n";
+  // Simple statment with new line.
+}
