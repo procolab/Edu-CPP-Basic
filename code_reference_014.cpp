247a3c77034bfa8d6a94615194b109a1434106b8
Himanshu <37922176+procolab@users.noreply.github.com>
2025-03-03 01:37:29 +0530

    Create code_reference_014.cpp
    Code for code_reference_014.cpp

---
+++ code_reference_014.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string food = "Pizza";
+  string &meal = food;
+
+  cout << food << "\n";
+  cout << meal << "\n";
+  
+  cout << &food; // Outputs like 0x6dfed4
+}
