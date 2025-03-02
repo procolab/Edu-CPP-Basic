01c3645d1e5516e9e659dc229cbd0be365835183
Himanshu <37922176+procolab@users.noreply.github.com>
2025-03-02 18:59:52 +0530

    Create code_variables_003.cpp
    code for code_variables_003.cpp

---
+++ code_variables_003.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myNum = 5; // Integer (whole number without decimals)
+  cout << "Value is: " << myNum << " Type is Integer.\n";
+  double myFloatNum = 3.14; // Floating point number (with decimals)
+  cout << "Value is: " << myFloatNum << " Type is Double.\n";
+  char myLetter = 'D'; // Character
+  cout << "Value is: " << myLetter << " Type is Character.\n";
+  string myText = "Hello"; // String (text)
+  cout << "Value is: " << myText << " Type is String.\n";
+  bool myBoolean = true; // Boolean (true or false)
+  cout << "Value is: " << myBoolean << " Type is Boolean.\n";
+}
