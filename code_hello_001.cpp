314368d685c2ff2fbefa7196790a881e04582bf2
Himanshu <37922176+procolab@users.noreply.github.com>
2025-03-02 18:30:01 +0530

    Create code_hello_001.cpp
    Hello World!
    === Code Execution Successful ===

---
+++ code_hello_001.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+// header file library that lets us work with input and output objects, such as cout.
+
+using namespace std;
+// means that we can use names for objects and variables from the standard library.
+
+int main() {
+  // This is called a function.
+  
+  cout << "Hello World!";
+  // insertion operator (<<) to output/print text.
+}
